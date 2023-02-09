cecb6f8684da9b779ad0073b94d85480fff3041a
João Pedro <11joao44@gmail.com>
2023-02-08 22:42:52 -0300

    commit initial

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,38 @@
 AOS.init();
 
-const dataEvent = new Date("Feb 10, 2023 03:00:00");
+const dataEvent = new Date("Jul 3, 2023 00:00:00");
+const dataPromo = new Date("Mar 3, 2023 00:00:00");
+
 const timeStampEvent = dataEvent.getTime();
+const timeStampPromo = dataPromo.getTime();
+
+const promo = setInterval(function() {
+    const now = new Date()
+    const timeStampCurrent = now.getTime()
+
+    const timerPromo = timeStampPromo - timeStampCurrent;
+
+    const dayInMs = 1000 * 60 * 60 *24;
+    const hourInMs = 1000 * 60 * 60;
+    const minutesInMs = 1000 * 60;
+    const secondIsMs = 1000;
+
+    const dayDistaceEvent = Math.floor(timerPromo / dayInMs);
+    const hourstimerPromo = Math.floor((timerPromo % dayInMs) / hourInMs);
+    const minutestimerPromo = Math.floor((timerPromo % hourInMs) / minutesInMs);
+    const secondtimerPromo = Math.floor((timerPromo % minutesInMs) / secondIsMs)
+
+    document.getElementById('promo').innerHTML = `${dayDistaceEvent}d ${hourstimerPromo}h ${minutestimerPromo}m ${secondtimerPromo}s`
+
+    if (distaceEvent < 0) {
+        clearInterval(timer);
+
+        document.getElementById('promo').innerHTML = `Promoção expirou`
+    }
+
+}, 1000)
+
+
 
 const timer = setInterval(function() {
     const now = new Date()
